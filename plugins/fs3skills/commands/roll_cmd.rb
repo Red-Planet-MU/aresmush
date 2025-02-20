73c370e0a75d00e2b0e09f472ada8f83fedda7f0
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-02-20 16:27:34 -0500

    messaging debug

--- plugins/fs3skills/commands/roll_cmd.rb
+++ plugins/fs3skills/commands/roll_cmd.rb
@@ -41,7 +41,8 @@ module AresMUSH
         success_level = FS3Skills.get_success_level(die_result)
         success_title = FS3Skills.get_success_title(success_level)
         Global.logger.debug "You're in the right place. Success level: #{success_level}"
-        if success_level == -1 || success_level == 16
+        if success_level == -1 
+          Global.logger.debug "It happened"
           message = t('fs3skills.auto_roll_result', 
             :name => char ? char.name : "#{self.name} (#{enactor_name})",
             :roll => self.roll_str,
