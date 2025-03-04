fecf882030c28d8540c9a3707f2466237a2e55a3
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-03-04 14:17:43 -0500

    serum use command

---
+++ plugins/serum/commands/serum_use_cmd.rb
@@ -0,0 +1,36 @@
+module AresMUSH
+    module Serum
+      class SerumUseCommand
+        include CommandHandler
+  
+        attr_accessor :serum_name, :char, :serum_type, :serum_has, :target, :combat_only_serum
+  
+        def parse_args
+          args = cmd.parse_args(ArgParser.arg1_equals_optional_arg2)
+          self.serum_name = titlecase_arg(args.arg1)
+          self.char = titlecase_arg(args.arg2)
+          self.serum_type = Serum.find_serums_type(self.serum_name)
+          self.serum_has = Serum.find_serums_has(enactor, self.serum_name)
+          if self.char
+            self.target = Character.find_one_by_name(self.char)
+          else
+            self.target = enactor
+          end
+          self.combat_only_serum = Global.read_config('serum',self.serum_name,'combat_only')
+        end
+  
+        def check_errors
+          return t('serum.dont_have_serum') if Serum.find_serums_has(enactor, self.serum_name) < 1
+          Global.logger.debug "Combat only: #{self.combat_only_serum}"
+          return t('serum.not_in_combat') if self.combat_only_serum == true && enactor.combat
+        end      
+  
+        def handle
+          ClassTargetFinder.with_a_character(self.target_name, client, enactor) do |model|
+             template = SerumTemplate.new(model)
+             client.emit template.render
+          end
+        end
+      end
+    end
+  end
