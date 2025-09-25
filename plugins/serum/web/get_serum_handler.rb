d1b57d1afaf4f2cb0cf7e006aaec72ccedafe101
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-09-25 13:35:40 -0400

    missing end

--- plugins/serum/web/get_serum_handler.rb
+++ plugins/serum/web/get_serum_handler.rb
@@ -12,6 +12,7 @@ module AresMUSH
           return error if error
           if comper_id == char_name_or_id
             return { error: t('compliments.cant_comp_self') }
+          end
         end
       end
     end
