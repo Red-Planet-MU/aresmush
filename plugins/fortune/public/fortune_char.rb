8154a9b038718e0a9bed28f486403fff7b8b86a6
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-10-21 15:34:20 -0400

    Update fortune_char.rb

--- plugins/fortune/public/fortune_char.rb
+++ plugins/fortune/public/fortune_char.rb
@@ -1,6 +1,8 @@
 module AresMUSH
   class Character
     attribute :fortune_cooldown_expires_at, :type => Ohm::DataTypes::DataType::Time
+    attribute :fortunes_told_alltime
+    attribute :fortunes_told_for_cron
 
   end
 end
