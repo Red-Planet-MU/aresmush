3e8d0ea67291ae0bfbde112c85cb441ba30c0884
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-11-22 09:24:36 -0500

    debugging

--- plugins/socializer/templates/pals_template.rb
+++ plugins/socializer/templates/pals_template.rb
@@ -6,10 +6,12 @@ module AresMUSH
       
       def initialize(enactor)
         @enactor = enactor
+        Global.logger.debug "enactor: #{@enactor}, pals: #{@enactor.pals}"
         super File.dirname(__FILE__) + "/pals.erb"
       end
       
       def pals_list
+        Global.logger.debug "enactor: #{@enactor}, pals: #{@enactor.pals}"
         return t('global.none') if @enactor.pals.empty?
         Global.logger.debug "enactor: #{@enactor}, pals: #{@enactor.pals}"
         @enactor.pals.map { |p| p.name }.sort.join(", ")
