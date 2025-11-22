18eac1f51a20d5a0365546bffd92653c85cc3ddb
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-11-22 16:21:01 -0500

    testing

--- plugins/horse/helpers/horse_combat_helper.rb
+++ plugins/horse/helpers/horse_combat_helper.rb
@@ -4,9 +4,10 @@ module AresMUSH
     def self.horse_new_turn(combatant)
       #Check if mounted and unspooked
       if (combatant.mount_type && combatant.spook_counter == 0)
-        #Get the spook rating
+        #Get the spook rating and horse bond
         spook_rating = Global.read_config('horse', 'spook_rating')
-        going_to_spook = rand(1...spook_rating+1)
+        bond_with_horse = combatant.associated_model.horse_bond
+        going_to_spook = rand(1...spook_rating + 1 + bond_with_horse)
         
         Global.logger.debug "going_to_spook: #{going_to_spook}, spook_rating: #{spook_rating}"
         #Check if random number matches spook rating
