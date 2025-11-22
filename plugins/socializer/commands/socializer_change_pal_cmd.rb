492c2258070b33310e3618297df135203318892a
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-11-21 22:02:56 -0500

    Pals List

--- plugins/socializer/commands/socializer_change_pal_cmd.rb
+++ plugins/socializer/commands/socializer_change_pal_cmd.rb
@@ -18,21 +18,21 @@ module AresMUSH
           
           ClassTargetFinder.with_a_character(self.pal, client, enactor) do |model|
           if (self.add_pal)
-            if (character.pals.include?(model))
+            if (enactor.pals.include?(model))
               client.emit_failure t('socializer.pal_already_exists', :name => model.name)
               return
             end
-            character.pals.add model
+            enactor.pals.add model
             message = t('socializer.pal_added', :name => enactor.name, :pal => model.name)
             #Add something to emit the message
             client.emit_success(message)
             #Jobs.notify(job, message, enactor)
           else
-            if (!char.pals.include?(model))
+            if (!enactor.pals.include?(model))
               client.emit_failure t('socializer.pal_doesnt_exist', :name => model.name)
               return
             end
-            char.pals.delete model
+            enactor.pals.delete model
             message = t('socializer.pal_removed', :name => enactor.name, :pal => model.name)
             client.emit_success(message)
             #add something to emit the message
