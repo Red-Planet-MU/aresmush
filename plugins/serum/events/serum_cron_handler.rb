dfec50728a84fc638e7856cfab9601711283e531
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-11-23 11:08:17 -0500

    Periodic Revitalizer Serum

---
+++ plugins/serum/events/serum_cron_handler.rb
@@ -0,0 +1,20 @@
+module AresMUSH
+  module Serum    
+    class SerumCronHandler
+      def on_event(event)
+        config = Global.read_config("serum", "serum_cron")
+        return if !Cron.is_cron_match?(config, event.time)
+        
+        Global.logger.debug "Serum distribution."
+        
+        periodic_serum = Global.read_config("serum", "periodic_serum")
+        #max_xp = Global.read_config("fs3skills", "max_xp_hoard")
+        
+        approved = Chargen.approved_chars
+        approved.each do |a|
+          Serum.modify_serum(a, periodic_serum, 1)
+        end
+      end
+    end    
+  end
+end
