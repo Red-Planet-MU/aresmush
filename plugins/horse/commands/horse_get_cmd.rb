7d25791f068b882217b3238f9a72e3ab4d510643
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-12-04 16:31:08 -0500

    get values for message

--- plugins/horse/commands/horse_get_cmd.rb
+++ plugins/horse/commands/horse_get_cmd.rb
@@ -13,8 +13,13 @@ module AresMUSH
             enactor.spend_luck(1)
           end
           Horse.generate_horse(enactor)
+
+          horse_name = enactor.demographic("steed")
+          horse_color = enactor.horse_color
+          horse_temperament = enactor.horse_temperament
           client.emit_success t('horse.got_horse', :horse_name => horse_name, :horse_color => horse_color, :horse_temperament => horse_temperament)
           
+
         end
       end
     end
