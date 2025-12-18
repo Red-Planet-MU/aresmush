25373ab0b2f18c6851d78304973912a36c0095ce
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-12-18 16:26:45 -0500

    testing

--- plugins/lookingforrp/web/looking_for_rp_request_handler.rb
+++ plugins/lookingforrp/web/looking_for_rp_request_handler.rb
@@ -3,9 +3,27 @@ module AresMUSH
     class LookingForRpRequestHandler
       def handle(request)
 
+        #{
+        #  chars: LookingForRp.chars_looking_for_rp
+        #}
+        lfrp = {}
+    
+        LookingForRp.chars_looking_for_rp.each do |char|
+          lfrp[char.name] = build_web_lfrp_data(char)
+        end
+          
         {
-          chars: LookingForRp.chars_looking_for_rp
+          lfrp: lfrp.values.sort_by { |v| v[:name] }
         }
+      end
+
+      def build_web_lfrp_data(char)
+        {
+          name: char.name,
+          icon: Website.icon_for_char(char),
+          status: Website.activity_status(char)
+        }
+      end
 
       end
     end
