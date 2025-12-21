aae507aec8e11f67d5286b4765f052a2d08af0bb
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-12-21 17:12:46 -0500

    testing

--- plugins/serum/web/get_serum_handler.rb
+++ plugins/serum/web/get_serum_handler.rb
@@ -6,8 +6,9 @@ module AresMUSH
           char = Character.find_one_by_name(char_name_or_id)
           serum_name = request.args['serum_type']
           enactor = request.enactor
-          enactor_char = Character[enactor]
-          Global.logger.debug "char: #{char} serum_name: #{serum_name} enactor: #{enactor} enactor_char: #{enactor_char}"
+          enactor_char = Character(enactor)
+          Global.logger.debug "char: #{char} serum_name: #{serum_name} enactor: #{enactor} enactor_char: #{enactor_char}, 
+          char_name_or_id = #{char_name_or_id}"
           Serum.modify_serum(char, serum_name, 1)
           char.spend_luck(1)
           error = Website.check_login(request)
