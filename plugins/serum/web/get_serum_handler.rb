0ee245cd9a8b520bf1eaf70388ced9a3d7231d01
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-12-21 17:19:10 -0500

    move error in order of operations

--- plugins/serum/web/get_serum_handler.rb
+++ plugins/serum/web/get_serum_handler.rb
@@ -8,13 +8,15 @@ module AresMUSH
           serum_name = request.args['serum_type']
           enactor = request.enactor
           Global.logger.debug "char: #{char} serum_name: #{serum_name} enactor: #{enactor}  char_name_or_id = #{char_name_or_id} char2: #{char2}"
-          Serum.modify_serum(char, serum_name, 1)
-          char.spend_luck(1)
           error = Website.check_login(request)
           return error if error
           if enactor != char_name_or_id
             return { error: t('serum.cant_get_serum_for_others') }
           end
+          Serum.modify_serum(char, serum_name, 1)
+          char.spend_luck(1)
+          error = Website.check_login(request)
+          
         end
       end
     end
