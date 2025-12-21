cdd49b5cbe6999a0abc0833485cc5049600cc2af
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2025-12-21 17:27:02 -0500

    testing

--- plugins/serum/web/get_serum_handler.rb
+++ plugins/serum/web/get_serum_handler.rb
@@ -4,22 +4,15 @@ module AresMUSH
         def handle(request)
           char_name_or_id = request.args['char_id']
           char = Character.find_one_by_name(char_name_or_id)
-          char2 = Character.named(request.args['char'])
           serum_name = request.args['serum_type']
           enactor = request.enactor
-          enactor_name = enactor.name
-          char_name = char.name
-          char2_name = char2.name
-          Global.logger.debug "char: #{char} serum_name: #{serum_name} enactor: #{enactor}  char_name_or_id = #{char_name_or_id} char2: #{char2}"
-          Global.logger.debug "enactor_name: #{enactor_name} char_name: #{char_name} char2_name: #{char2_name}"
           error = Website.check_login(request)
           return error if error
-          if enactor != char_name_or_id
+          if enactor.name != char_name_or_id.name
             return { error: t('serum.cant_get_serum_for_others') }
           end
           Serum.modify_serum(char, serum_name, 1)
           char.spend_luck(1)
-          error = Website.check_login(request)
           
         end
       end
