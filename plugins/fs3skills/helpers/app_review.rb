e3c1e98bf61c8fb37312c5381c9bfc55e043edbc
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-01-05 11:25:06 -0500

    wrong line

--- plugins/fs3skills/helpers/app_review.rb
+++ plugins/fs3skills/helpers/app_review.rb
@@ -125,18 +125,18 @@ module AresMUSH
         specs_for_skill = starting_specs[a.name]
         if (specs_for_skill)
           specs_for_skill.each do |s|
-            #if (!a.specialties.include?(s))        
-            #  missing << t('fs3skills.missing_group_specialty', :spec => s, :skill => a.name)
-            #end
+            if (!a.specialties.include?(s))        
+              missing << t('fs3skills.missing_group_specialty', :spec => s, :skill => a.name)
+            end
           end
         end
       end
       
       char.fs3_action_skills.each do |a|
         config = FS3Skills.action_skill_config(a.name)
-        if (config['specialties'] && a.specialties.empty? && a.rating > 2)
-          missing << t('fs3skills.missing_specialty', :skill => a.name)
-        end
+        #if (config['specialties'] && a.specialties.empty? && a.rating > 2)
+        #  missing << t('fs3skills.missing_specialty', :skill => a.name)
+        #end
       end
       
       
