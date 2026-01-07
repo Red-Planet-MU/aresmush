66f3dac4d785558f492b88cc90bfda4290947877
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-01-07 16:58:19 -0500

    track fortunes on a cooldown

--- plugins/fortune/commands/fortune_cmd.rb
+++ plugins/fortune/commands/fortune_cmd.rb
@@ -11,13 +11,14 @@ module AresMUSH
 
       def check_errors
         return t('fortune.no_machine') if enactor.room.can_use_fortune != true
-        puts "Getting a fortune."
+        return t('fortune.cooldown_on') if enactor.fortunes_told_lately > 3
       end
 
       def handle
         
         fortune_to_tell = Fortune.get_fortune()
-
+        enactor.update(fortunes_told_lately: fortunes_told_lately + 1)
+        enactor.update(fortunes_told_alltime: fortunes_told_alltime + 1)
         message = t('fortune.told_fortune', :name => enactor.name, :fortune_told => fortune_to_tell)
         enactor.room.emit message
         if enactor.room.scene
--- plugins/fortune/public/fortune_char.rb
+++ plugins/fortune/public/fortune_char.rb
@@ -1,8 +1,7 @@
 module AresMUSH
   class Character
-    attribute :fortune_cooldown_expires_at, :type => Ohm::DataTypes::DataType::Time
     attribute :fortunes_told_alltime
-    attribute :fortunes_told_for_cron
+    attribute :fortunes_told_lately
 
   end
 end
