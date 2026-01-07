eecabfac31c5585170e3d8d3eda6de552e861a79
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-01-07 17:13:29 -0500

    testing

--- plugins/fortune/commands/fortune_cmd.rb
+++ plugins/fortune/commands/fortune_cmd.rb
@@ -7,35 +7,15 @@ module AresMUSH
 
       def parse_args
         #self.fortune = titlecase_arg(cmd.args)
-        if !enactor.fortunes_told_alltime
-          fortunes_told_lately = 0
-          fortunes_told_alltime = 0
-        else
-          fortunes_told_lately = enactor.fortunes_told_lately
-          fortunes_told_alltime = enactor.fortunes_told_alltime
-        end
+
       end
 
       def check_errors
-        if !enactor.fortunes_told_alltime
-          fortunes_told_lately = 0
-          fortunes_told_alltime = 0
-        else
-          fortunes_told_lately = enactor.fortunes_told_lately
-          fortunes_told_alltime = enactor.fortunes_told_alltime
-        end
         return t('fortune.no_machine') if enactor.room.can_use_fortune != true
         return t('fortune.cooldown_on') if fortunes_told_lately > 3
       end
 
       def handle
-        if !enactor.fortunes_told_alltime
-          fortunes_told_lately = 0
-          fortunes_told_alltime = 0
-        else
-          fortunes_told_lately = enactor.fortunes_told_lately
-          fortunes_told_alltime = enactor.fortunes_told_alltime
-        end
         fortune_to_tell = Fortune.get_fortune()
         enactor.update(fortunes_told_lately: fortunes_told_lately + 1)
         enactor.update(fortunes_told_alltime: fortunes_told_alltime + 1)
