68c6440787d35305476e0bafe2d5a30d052808eb
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-01-07 16:19:47 -0500

    order

--- plugins/socializer/web/web_pals_add_handler.rb
+++ plugins/socializer/web/web_pals_add_handler.rb
@@ -9,12 +9,12 @@ module AresMUSH
         error = Website.check_login(request)
         return error if error
         
-        if (enactor.pals.include?(target))
-          return { error: t('socializer.pal_already_exists', :name => target.name) }
-        end
-
         if !target 
           return { error: t('socializer.no_such_pal', :name => target.name) }
+        end
+
+        if (enactor.pals.include?(target))
+          return { error: t('socializer.pal_already_exists', :name => target.name) }
         end
 
         enactor.pals.add target
