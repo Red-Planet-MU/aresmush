84ab09b56d53bbbddf5adfe4eb8c38e726362dd7
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-01-09 13:29:49 -0500

    debugging

--- plugins/fortune/web/fortune_request_handler.rb
+++ plugins/fortune/web/fortune_request_handler.rb
@@ -21,12 +21,16 @@ module AresMUSH
         enactor.update(fortunes_told_lately: enactor.fortunes_told_lately + 1)
         enactor.update(fortunes_told_alltime: enactor.fortunes_told_alltime + 1)
         scene_message = t('fortune.told_fortune', :name => enactor.name, :fortune_told => fortune_to_tell)
+        Global.logger.debug "scene_message: #{scene_message}"
         Fortune.handle_fortune_given_achievement(enactor)
+        Global.logger.debug "Not broken yet"
         Scenes.add_to_scene(scene, scene_message)
+        Global.logger.debug "Not broken yet 2"
         if enactor.room.scene
           enactor.room.emit scene_message
+          Global.logger.debug "Not broken yet 3"
         end
-
+        Global.logger.debug "Not broken yet 4"
       end
     end
   end
