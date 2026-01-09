4ac2894aa8d8a60730b2ab9f18f17491087a8140
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-01-09 15:58:03 -0500

    testing

--- plugins/fs3combat/commands/gear/combat_weapon_cmd.rb
+++ plugins/fs3combat/commands/gear/combat_weapon_cmd.rb
@@ -44,7 +44,9 @@ module AresMUSH
 
       def check_pc_valid_weapon
         self.names.each do |n|
-          return t('fs3combat.npc_only_weapon') if !FS3Combat.pc_equippable_weapon(self.weapon) && !n.is_npc?
+          FS3Combat.with_a_combatant(n, client, enactor) do |combat, combatant| 
+            return t('fs3combat.npc_only_weapon') if !FS3Combat.pc_equippable_weapon(self.weapon) && !n.is_npc?
+          end
         end
         return nil
       end
