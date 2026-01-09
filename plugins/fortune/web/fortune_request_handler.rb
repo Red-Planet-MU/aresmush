b294fbd3a8cc8e6a51bb8b873ae906f428e07f7e
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-01-09 13:37:23 -0500

    testing

--- plugins/fortune/web/fortune_request_handler.rb
+++ plugins/fortune/web/fortune_request_handler.rb
@@ -8,8 +8,6 @@ module AresMUSH
         error = Website.check_login(request)
         scene_id = request.args['id']
         scene = Scene[request.args['id']]
-
-        Global.logger.debug "scene: #{scene}, id: #{scene_id}, enactor: #{enactor}"
           
         return error if error
         if enactor.fortunes_told_lately >= 3
@@ -17,20 +15,15 @@ module AresMUSH
         end
         fortune_to_tell = Fortune.get_fortune()
 
-        Global.logger.debug "fortune_to_tell: #{fortune_to_tell}"
         enactor.update(fortunes_told_lately: enactor.fortunes_told_lately + 1)
         enactor.update(fortunes_told_alltime: enactor.fortunes_told_alltime + 1)
         scene_message = t('fortune.told_fortune', :name => enactor.name, :fortune_told => fortune_to_tell)
-        Global.logger.debug "scene_message: #{scene_message}"
         Fortune.handle_fortune_given_achievement(enactor)
-        Global.logger.debug "Not broken yet"
         Scenes.add_to_scene(scene, scene_message)
-        Global.logger.debug "Not broken yet 2"
         if enactor.room.scene
           enactor.room.emit scene_message
-          Global.logger.debug "Not broken yet 3"
         end
-        Global.logger.debug "Not broken yet 4"
+        Global.logger.debug "For some reason the fortune code requires me to be here"
       end
     end
   end
