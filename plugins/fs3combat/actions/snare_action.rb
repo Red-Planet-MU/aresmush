b77e3435a8489afe93abea0e12b0a29f3e493b83
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-01-09 21:34:06 -0500

    typo

--- plugins/fs3combat/actions/snare_action.rb
+++ plugins/fs3combat/actions/snare_action.rb
@@ -35,7 +35,7 @@ module AresMUSH
         #end
         if FS3Combat.weapon_stat(self.combatant.weapon, "damage_type") == "Stun"
           is_snare = true
-          if FS3Skills.find_specialty(combatant.character, "Trapping") == "Snares"
+          if FS3Skills.find_specialty(self.combatant.character, "Trapping") == "Snares"
           snare_spec_boost = 1
           else 
           snare_spec_boost = 0
