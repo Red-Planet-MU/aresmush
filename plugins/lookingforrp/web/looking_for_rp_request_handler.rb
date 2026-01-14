e05c4cea1df37cb5a7721bfc23e10e90a9e03905
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-01-14 14:26:07 -0500

    styling

--- plugins/lookingforrp/web/looking_for_rp_request_handler.rb
+++ plugins/lookingforrp/web/looking_for_rp_request_handler.rb
@@ -4,7 +4,9 @@ module AresMUSH
       def handle(request)
 
         {
-          chars: LookingForRp.web_list
+        looking_for_rp: LookingForRp.char_names,
+        looking_for_rp_chars: LookingForRp.chars_looking_for_rp,
+        lfrp_icons: LookingForRp.web_list
         }
 
       end
