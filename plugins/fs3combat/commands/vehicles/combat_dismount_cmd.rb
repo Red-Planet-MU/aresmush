9a75ee7fd0e171378bc53afe74d4c6c2ee2cb322
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-01-14 09:43:49 -0500

    I should already have combatant

--- plugins/fs3combat/commands/vehicles/combat_dismount_cmd.rb
+++ plugins/fs3combat/commands/vehicles/combat_dismount_cmd.rb
@@ -29,12 +29,15 @@ module AresMUSH
             combatant.update(mount_type: nil)
             FS3Combat.emit_to_combat combat, t('fs3combat.dismounted', :name => combatant.name)
           elsif (combatant.mount_type && combatant.is_riding_with)
+            combatant2 = combatant.is_riding_with
             combatant.update(mount_type: nil)
             combatant.update(is_riding_with: nil)
-            FS3Combat.with_a_combatant(target, client, enactor) do |combat2, combatant2|
-              combatant2.update(is_carrying: nil)
-              FS3Combat.emit_to_combat combat, t('fs3combat.dismounted_from_ride', :name => combatant.name, :ride_giver => combatant2.name)
-            end
+            combatant2.update(is_carrying: nil)
+            FS3Combat.emit_to_combat combat, t('fs3combat.dismounted_from_ride', :name => combatant.name, :ride_giver => combatant2.name)
+            #FS3Combat.with_a_combatant(target, client, enactor) do |combat2, combatant2|
+            #  combatant2.update(is_carrying: nil)
+            #  FS3Combat.emit_to_combat combat, t('fs3combat.dismounted_from_ride', :name => combatant.name, :ride_giver => combatant2.name)
+            #end
           else
             client.emit_failure t('fs3combat.not_mounted')
           end
