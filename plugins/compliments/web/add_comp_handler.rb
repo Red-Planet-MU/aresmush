e4e7c0f38a04920b3152b1022107007e589dd8a1
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-01-20 16:19:05 -0500

    update comp handler again

--- plugins/compliments/web/add_comp_handler.rb
+++ plugins/compliments/web/add_comp_handler.rb
@@ -7,7 +7,7 @@ module AresMUSH
         char_name_or_id = request.args['char_id']
         char = Character.find_one_by_name(char_name_or_id)
         comper_id = request.auth[:id]
-        Compliments.add_comp([char], comp_msg, Character[comper_id])
+        Compliments.add_comp(char, comp_msg, Character[comper_id])
         error = Website.check_login(request)
         return error if error
         if comper_id == char_name_or_id
