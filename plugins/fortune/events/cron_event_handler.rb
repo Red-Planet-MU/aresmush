d965a3e7904237c8786d8d9431a842885c9af623
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-01-30 12:47:04 -0500

    cron for books

--- plugins/fortune/events/cron_event_handler.rb
+++ plugins/fortune/events/cron_event_handler.rb
@@ -6,11 +6,18 @@ module AresMUSH
         fortune_cron = Global.read_config("fortune", "cron")
        if Cron.is_cron_match?(fortune_cron, event.time)
 
-          chars = Chargen.approved_chars.select { |c| c.fortunes_told_lately > 0 }
+          fortune_chars = Chargen.approved_chars.select { |c| c.fortunes_told_lately > 0 }
 
-          chars.each do |c|
+          fortune_chars.each do |c|
             Global.logger.debug "Expiring fortune cooldown for #{c.name}"
             c.update(fortunes_told_lately: c.fortunes_told_lately - 1)
+          end
+
+          book_chars = Chargen.approved_chars.select { |c| c.books_got_lately > 0 }
+
+          book_chars.each do |c|
+            Global.logger.debug "Expiring book cooldown for #{c.name}"
+            c.update(books_got_lately: c.books_got_lately - 1)
           end
         end
       end
