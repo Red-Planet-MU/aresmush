88f46303ab2da85dceffa24f91076fec27e591e2
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-03-05 11:15:23 -0500

    mines can explode on you

--- plugins/fs3combat/actions/explode_action.rb
+++ plugins/fs3combat/actions/explode_action.rb
@@ -25,19 +25,49 @@ module AresMUSH
       
       def resolve
         messages = []
-        
-        messages << t('fs3combat.explode_resolution_message', :name => self.name, :weapon => self.combatant.weapon)
-        
-        self.targets.each do |target, num|
-          messages.concat FS3Combat.resolve_explosion(combatant, target)
+        if FS3Combat.weapon_stat(self.combatant.weapon, "trap_type") == "Explosives"
+          is_trap = true
+        else
+          is_trap = false
         end
+        
+        if is_trap == true
+          ability = FS3Combat.weapon_stat(combatant.weapon, "skill")
+          if combatant.is_npc? == false then
+            #Find if weapon has specialty to add to attack roll
+            firearm_specialty = FS3Combat.weapon_stat(combatant.weapon, "firearm_type")
+            trapping_specialty = FS3Combat.weapon_stat(combatant.weapon, "trap_type")
+            #then need to find if character has that specialty
+            combatant_specialty = FS3Skills.find_specialty(combatant.character, ability)
+        
+            #If weapon and character's specialty match then give a 1 boost to attack roll
+            if firearm_specialty == combatant_specialty || trapping_specialty == combatant_specialty then specialty_mod = 1
+            else
+              specialty_mod = 0
+            end
+          else specialty_mod = 0
+          end
+          botch_roll = combatant.roll_ability(ability, specialty_mod)
+          successes = TDD.get_success_level(botch_roll)
+          if successes < 0
+            messages << t('fs3combat.explode_self_message', :name => self.name, :weapon => self.combatant.weapon)
+            messages.concat FS3Combat.resolve_explosion(combatant, combatant)
+            messages
+          end
+        else
+          messages << t('fs3combat.explode_resolution_message', :name => self.name, :weapon => self.combatant.weapon)
+        
+          self.targets.each do |target, num|
+            messages.concat FS3Combat.resolve_explosion(combatant, target)
+          end
 
-        ammo_message = FS3Combat.update_ammo(@combatant, 1)
-        if (ammo_message)
-          messages << ammo_message
-        end
+          ammo_message = FS3Combat.update_ammo(@combatant, 1)
+          if (ammo_message)
+            messages << ammo_message
+          end
         
-        messages
+          messages
+        end
       end
     end
   end
