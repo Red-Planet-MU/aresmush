c033330e586d7e0dcc8581461cef7752d82205d0
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-04-09 20:53:57 -0400

    debugging web comps

--- plugins/compliments/web/get_comps_handler.rb
+++ plugins/compliments/web/get_comps_handler.rb
@@ -5,7 +5,7 @@ module AresMUSH
         page = (request.args[:page] || "1").to_i
         char_name_or_id = request.args[:char_id]
         char = Character.find_one_by_name(char_name_or_id)
-        puts "Char: #{char} Page #{page}"
+        Global.logger.debug "Char: #{char} Page #{page}"
         puts Compliments.get_comps(char, page)
         Compliments.get_comps(char, page)
 
