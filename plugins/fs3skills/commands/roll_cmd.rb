a6a8bc537c20113a9ab79d10570d625a7f417ac3
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-04-12 17:46:55 -0400

    man I thought we had that working

--- plugins/fs3skills/commands/roll_cmd.rb
+++ plugins/fs3skills/commands/roll_cmd.rb
@@ -40,12 +40,20 @@ module AresMUSH
         
         success_level = FS3Skills.get_success_level(die_result)
         success_title = FS3Skills.get_success_title(success_level)
-        message = t('fs3skills.simple_roll_result', 
-          :name => char ? char.name : "#{self.name} (#{enactor_name})",
-          :roll => self.roll_str,
-          :dice => FS3Skills.print_dice(die_result),
-          :success => success_title
-        )
+        if success_level == -1 || success_level == 16
+          message = t('tdd.auto_roll_result', 
+            :name => char ? char.name : "#{self.name} (#{enactor_name})",
+            :roll => self.roll_str,
+            :success => success_title
+          )
+        else
+          message = t('fs3skills.simple_roll_result', 
+            :name => char ? char.name : "#{self.name} (#{enactor_name})",
+            :roll => self.roll_str,
+            :dice => TDD.print_dice(die_result),
+            :success => success_title
+          )
+        end
         FS3Skills.emit_results message, client, enactor_room, self.private_roll
       end
     end
