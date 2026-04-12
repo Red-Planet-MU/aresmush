c8fd535b9f9c9eff85f386b08f056c8765effe5b
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-04-12 16:06:35 -0400

    typo

--- plugins/fs3combat/web/heal_scan_request_handler.rb
+++ plugins/fs3combat/web/heal_scan_request_handler.rb
@@ -20,7 +20,7 @@ module AresMUSH
         damaged_chars = Chargen.approved_chars.select { |c| FS3Combat.total_damage_mod(c) < 0}
 
 
-        heal_scan_chars = damaged_chars.sort_by { |pc| FS3Combat.total_damage_mod(c)}.map { |c| { name: c.name, icon: Website.icon_for_char(c), damage_mod: FS3Combat.total_damage_mod(c) } }
+        heal_scan_chars = damaged_chars.sort_by { |pc| FS3Combat.total_damage_mod(pc)}.map { |c| { name: c.name, icon: Website.icon_for_char(c), damage_mod: FS3Combat.total_damage_mod(c) } }
         
         {heal_scan_list: heal_scan_chars}
       end
