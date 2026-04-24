833ca821c84dc495c81ff56fa30cee025092cc19
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-04-24 08:47:02 -0400

    is this stupid

--- plugins/serum/web/get_serum_handler.rb
+++ plugins/serum/web/get_serum_handler.rb
@@ -15,8 +15,8 @@ module AresMUSH
             return { error: t('fs3skills.not_enough_points') }
           end
           Serum.modify_serum(char, serum_name, 1)
-          enactor.update(serums_bought: enactor.serums_bought + 1)
-          Serum.handle_serum_obtained_given_achievement(enactor)
+          char.update(serums_bought: char.serums_bought + 1)
+          Serum.handle_serum_obtained_given_achievement(char)
           char.spend_luck(1)
           
         end
