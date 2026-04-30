a31a8ac063eed70cab33765b238aaa93eb6b24eb
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-04-30 12:48:22 -0400

    lfrp txt handling for web

--- plugins/lookingforrp/web/setLFRP_handler.rb
+++ plugins/lookingforrp/web/setLFRP_handler.rb
@@ -5,15 +5,22 @@ module AresMUSH
         puts request.args
         enactor = request.enactor
         time = request.args['hours'].to_i
+        type = request.args['textType']
 
-
-        LookingForRp.set(enactor, time)
-        if enactor.looking_for_rp_announce == "on"
-          Channels.send_to_channel("RP Requests", t('lookingforrp.rp_request_emit', :name => enactor.name, :duration => time))
-        end
         error = Website.check_login(request)
         return error if error
 
+        if !type
+          LookingForRp.set(enactor, time)
+          if enactor.looking_for_rp_announce == "on"
+            Channels.send_to_channel("RP Requests", t('lookingforrp.rp_request_emit', :name => enactor.name, :duration => time))
+          end
+        else
+          LookingForRp.set(enactor, time, "text")
+          if enactor.looking_for_rp_announce == "on"
+            Channels.send_to_channel("RP Requests", t('lookingforrp.rp_request_emit_txt', :name => enactor.name, :duration => time))
+          end
+        end
         {
         }
       end
