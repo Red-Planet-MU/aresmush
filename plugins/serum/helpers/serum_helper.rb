41c1cbd99089c90f2a2f9c066106754946bc3d6f
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-05-09 21:46:43 -0400

    try fixing NPC rolls

--- plugins/serum/helpers/serum_helper.rb
+++ plugins/serum/helpers/serum_helper.rb
@@ -56,7 +56,11 @@ module AresMUSH
       end
   
       def self.combat_healing_serum(char, target, serum_name)
-        heal_roll = TDD.parse_and_roll(char, "Medicine")
+        if char.is_npc?
+          heal_roll = char.roll_ability("Medicine")
+        else
+          heal_roll = TDD.parse_and_roll(char, "Medicine")
+        end
         heal_success_level = TDD.get_success_level(heal_roll)
         dice_message = TDD.print_dice(heal_roll)
         wound = FS3Combat.worst_serumable_wound(target)
