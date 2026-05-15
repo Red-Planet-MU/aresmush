450c4c7fc27d7d5da138383fc591d43a61fa65df
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-05-15 12:59:43 -0400

    testing

--- plugins/website/custom_web_data.rb
+++ plugins/website/custom_web_data.rb
@@ -15,7 +15,7 @@ module AresMUSH
     def self.custom_sidebar_data(viewer)
       return {
         looking_for_rp: LookingForRp.char_names,
-        looking_for_rp_chars: LookingForRp.chars_looking_for_rp,
+        #looking_for_rp_chars: LookingForRp.chars_looking_for_rp,
         lfrp_icons: LookingForRp.web_list
         #is_looking_for_rp: LookingForRp.is_lfrp(viewer)
       }
