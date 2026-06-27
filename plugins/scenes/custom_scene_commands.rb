4b4d9a3bf6bc438273e810ceb8290f5f5582d9ff
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-06-27 11:16:29 -0400

    testing

--- plugins/scenes/custom_scene_commands.rb
+++ plugins/scenes/custom_scene_commands.rb
@@ -2,6 +2,11 @@ module AresMUSH
   module Scenes
     class CustomSceneCommands
       def handle(enactor, char, scene, command, args)
+        case command
+        when 'combat'
+          Scenes.emit_pose(char, "#{char.name} dances.", false, true, nil, false, scene.room)
+          return { command_response: "You dance." }
+        end
         return nil
       end
     end
