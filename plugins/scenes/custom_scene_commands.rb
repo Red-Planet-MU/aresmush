77ab7b70d426c6caaca079e92573edb8a08a9285
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-06-28 20:08:06 -0400

    git rid of /combat dancing

--- plugins/scenes/custom_scene_commands.rb
+++ plugins/scenes/custom_scene_commands.rb
@@ -2,11 +2,7 @@ module AresMUSH
   module Scenes
     class CustomSceneCommands
       def handle(enactor, char, scene, command, args)
-        case command
-        when 'combat'
-          Scenes.emit_pose(char, "#{char.name} dances.", false, true, nil, false, scene.room)
-          return { command_response: "You dance." }
-        end
+        #put custom commands here
         return nil
       end
     end
