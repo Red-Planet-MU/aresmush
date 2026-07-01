305b4349e334d7e5510b0587932903a1fa72fec8
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-07-01 12:18:27 -0400

    testing better web pm title

--- plugins/page/helpers.rb
+++ plugins/page/helpers.rb
@@ -233,7 +233,7 @@ module AresMUSH
       is_unread = Page.is_thread_unread?(thread, enactor)
       {
          key: thread.id,
-         title: thread.title_customized(enactor),
+         title: thread.title_customized(enactor).sub(enactor.name, "").sub(" ", ", "),
          enabled: true,
          can_join: true,
          can_talk: true,
