e53fb7de8761974629114dfc14a81d3f296f36f8
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-07-13 08:55:03 -0400

    put client side ping back in

--- plugins/login/events/cron_event_handler.rb
+++ plugins/login/events/cron_event_handler.rb
@@ -4,9 +4,9 @@ module AresMUSH
       def on_event(event)
         # Ping on every cron event
         # Ping the web clients if I ever think they need it; right now I do not
-        #Global.client_monitor.web_clients.each do |client|
-        #  client.ping
-        #end
+        Global.client_monitor.web_clients.each do |client|
+          client.ping
+        end
 
         Global.client_monitor.client_to_char_map.each do |client, char| 
           if (char.login_keepalive)
