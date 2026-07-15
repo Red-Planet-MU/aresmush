35617c8d11e223017c5e014869f422e18dcd0a34
Trashcan <140090289+trashcan-mush@users.noreply.github.com>
2026-07-15 13:16:33 -0400

    testing

--- plugins/channels/helpers.rb
+++ plugins/channels/helpers.rb
@@ -391,7 +391,7 @@ module AresMUSH
       def self.brief_channel_message(message)
         if message.message[0, (" "+message.author.ooc_name+" says,").size] == " "+message.author.ooc_name+" says,"
           return message.message.sub(message.author.ooc_name+" says, ","")
-        elsif message.message[0, ("[D] "+message.author.ooc_name+" says,").size] == "[D] "+message.author.ooc_name+" says,"
+        elsif message.message[0, ("<p>[D] "+message.author.ooc_name+" says,").size] == "<p>[D] "+message.author.ooc_name+" says,"
           return message.message.sub(message.author.ooc_name+" says, ","")
         elsif message.message[0, ("<p>"+message.author.name+" says,").size] == "<p>"+message.author.name+" says,"
           return message.message.sub(message.author.name+" says, ","")
